3fbe085ac1e8af07d12098189490f2ba893121ad
jpabloallin <jpac.647@gmail.com>
2022-06-02 20:56:09 -0500

    Api requests on the Controller class

--- src/main/java/com/mongoexample/CandidateController.java
+++ src/main/java/com/mongoexample/CandidateController.java
@@ -11,16 +11,42 @@ import java.util.List;
 public class CandidateController {
     @Autowired
     private CandidateRepository candidateRepository;
-
     @GetMapping("get/candidates")
     public List<Candidate> getAllCandidates() {
         return candidateRepository.findAll();
     }
-
+    @GetMapping("candidate/{id}")
+    public Candidate getCandidate(@PathVariable String id) {
+        return candidateRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException());
+    }
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public class ResourceNotFoundException extends RuntimeException {
+        public ResourceNotFoundException() {}
+    }
     @PostMapping("add")
     @ResponseStatus(code = HttpStatus.CREATED)
     public Candidate addCandidate(@RequestBody Candidate candidate) {
         return candidateRepository.save(candidate);
+    }
+
+    @PutMapping("update/{id}")
+    public Candidate update(@PathVariable String id, @RequestBody Candidate updateCandidate) {
+        Candidate candidate = candidateRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException());
+        candidate.setName(updateCandidate.getName());
+        candidate.setExp(updateCandidate.getExp());
+        candidate.setEmail(updateCandidate.getEmail());
+        return candidateRepository.save(candidate);
+    }
+
+    @DeleteMapping("delete/{id}")
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    public void delete(@PathVariable String id) {
+        Candidate candidate = candidateRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException());
+        candidateRepository.delete(candidate);
+
     }
 
 }
